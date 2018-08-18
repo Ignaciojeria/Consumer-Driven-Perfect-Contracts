b48233aa99cbc088f6b7bd5a5b178f3451c8017a
Ignacio <you@example.com>
2018-08-18 20:35:27 -0300

    first commit

---
+++ productor/src/main/java/com/productor/ProductorApplication.java
@@ -0,0 +1,12 @@
+package com.productor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductorApplication.class, args);
+	}
+}
